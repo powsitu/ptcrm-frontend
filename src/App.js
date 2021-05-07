e6619cf9e2c9eb91f5c7b062df7738f659c19440
powsitu <sendtopow@gmail.com>
2021-05-07 12:37:09 +0200

    added background image from landing page and basic directory structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import("./App.css");
+
 function App() {
-  return (
-    <div className="App">
-      Let's start coding!!!
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
