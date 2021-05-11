1e3fe893c828670ad222cb36e1c3b729f88f623b
powsitu <sendtopow@gmail.com>
2021-05-11 22:46:39 +0200

    fixed so that the reservations page loads new items with no need to logout and login again

--- src/pages/userStuff/reservations.js
+++ src/pages/userStuff/reservations.js
@@ -12,6 +12,7 @@ export default function Reservations() {
 
   const { error, loading, data } = useQuery(MY_RESERVATIONS, {
     variables: { userId: currentUser },
+    fetchPolicy: "network-only",
   });
 
   useEffect(() => {
