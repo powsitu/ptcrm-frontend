1d5620e4ddbcd44c9b023668d1fe40fda3e77f80
powsitu <sendtopow@gmail.com>
2021-05-11 14:10:34 +0200

    changed the navigation bar to use material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-
+import NavigationBar from "./components/NavigationBar";
 import LogIn from "./pages/login";
 import SignUp from "./pages/signup";
 import homepage from "./pages/homepage";
-import NavigationBar from "./components/NavigationBar";
+import checkins from "./pages/userStuff/checkins";
+import feedback from "./pages/userStuff/feedback";
+import reservations from "./pages/userStuff/reservations";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <Switch>
         <Route path="/login" component={LogIn} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/checkins" component={checkins} />
+        <Route path="/feedback" component={feedback} />
+        <Route path="/reservations" component={reservations} />
         <Route exact path="/" component={homepage} />
       </Switch>
     </div>
