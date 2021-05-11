4e74c6a09d024919514c03c13e496bf733e31a88
powsitu <sendtopow@gmail.com>
2021-05-11 21:14:03 +0200

    loaded up query to search for all reservations for currently logged in user

--- src/pages/userStuff/reservations.js
+++ src/pages/userStuff/reservations.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectUserId } from "../../store/user/selectors";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { MY_RESERVATIONS } from "../../store/trainings/gql_trainings";
 
-export default function reservations() {
+export default function Reservations() {
+  const currentUser = useSelector(selectUserId);
+  const [reservations, set_reservations] = useState([]);
+
+  const { error, loading, data } = useQuery(MY_RESERVATIONS, {
+    variables: { userId: currentUser },
+  });
+
+  useEffect(() => {
+    if (data) {
+      set_reservations(data);
+    }
+  }, [data]);
+
   return (
     <div>
       This is gonna be the page for the users to check all their reservations
+      {console.log(data)}
     </div>
   );
 }
