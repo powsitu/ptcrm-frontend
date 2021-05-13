38591f68de40a460974041dbef6b9119ef6c1d43
powsitu <sendtopow@gmail.com>
2021-05-13 15:29:33 +0200

    added table that displays all different trainings types from the database

--- src/components/Tables/managetrainingtypes.js
+++ src/components/Tables/managetrainingtypes.js
@@ -1,38 +1,38 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import { useMutation } from "@apollo/react-hooks";
-import { REMOVE_TRAINING } from "../../store/trainings/gql_trainings";
+// import { useMutation } from "@apollo/react-hooks";
+// import { REMOVE_TRAINING } from "../../store/trainings/gql_trainings";
 
 export default function TrainingTypesTable({ data }) {
-  const [removeTraining] = useMutation(REMOVE_TRAINING);
+  // const [removeTraining] = useMutation(REMOVE_TRAINING);
 
-  async function clickRemoveTraining(trainingId) {
-    const response = await removeTraining({
-      variables: { trainingId: parseInt(trainingId) },
-    });
-  }
+  // async function clickRemoveTraining(trainingId) {
+  //   const response = await removeTraining({
+  //     variables: { trainingId: parseInt(trainingId) },
+  //   });
+  // }
 
   return (
     <table>
       <thead>
         <tr>
-          <th>Time</th>
-          <th>City</th>
-          <th>Training</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Intensity</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        {data.getTrainingThisDay.map((training) => {
+        {data.map((trainingType) => {
           return (
-            <tr key={training.id}>
-              <td>{training.time}</td>
-              <td>{training.place.city}</td>
-              <td>{training.trainingType.name}</td>
+            <tr key={trainingType.id}>
+              <td>{trainingType.name}</td>
+              <td>{trainingType.description}</td>
+              <td>{trainingType.intensity}</td>
               <td>
                 <Button
                   variant="contained"
-                  onClick={() => clickRemoveTraining(training.id)}
+                  // onClick={() => clickRemoveTraining(trainingType.id)}
                 >
                   Delete
                 </Button>
